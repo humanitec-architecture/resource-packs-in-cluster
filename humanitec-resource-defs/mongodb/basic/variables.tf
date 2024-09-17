b1cc4c5e699b0b925d7eac12caf823ccc45e2fd0
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-09-17 18:52:46 +0200

    Fixing tflint

--- humanitec-resource-defs/mongodb/basic/variables.tf
+++ humanitec-resource-defs/mongodb/basic/variables.tf
@@ -2,14 +2,3 @@ variable "prefix" {
   description = "Prefix for all resources"
   type        = string
 }
-
-variable "name" {
-  description = "Name of the deployment"
-  type        = string
-
-  # Deployment names shouldn't be > 47 https://pauldally.medium.com/why-you-try-to-keep-your-deployment-names-to-47-characters-or-less-1f93a848d34c
-  # 47 - 6 (prefix) = 41
-  default = <<EOT
-mongo-{{ "$${context.res.id}" | replace "." "-" | substr 0 41 }}
-EOT
-}
