73a276fdd67203b4ff366ef2b595508abda60704
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-02-17 13:27:43 -0500

    test-definitions for mysql

--- humanitec-resource-defs/mysql/basic/variables.tf
+++ humanitec-resource-defs/mysql/basic/variables.tf
@@ -2,14 +2,3 @@ variable "prefix" {
   description = "Prefix for all resources"
   type        = string
 }
-
-variable "name" {
-  description = "Name of the statefulset"
-  type        = string
-
-  # StatefulSets names are limited to 52 chars https://github.com/kubernetes/kubernetes/issues/64023
-  # 52 - 6 (prefix) = 46
-  default = <<EOT
-mysql-{{ "$${context.res.id}" | replace "." "-" | substr 0 46 }}
-EOT
-}
--- scripts/test-definitions.sh
+++ scripts/test-definitions.sh
@@ -4,23 +4,21 @@ set -o errexit
 templates=$(ls humanitec-resource-defs/)
 for template in $templates;
 do
-  if [ "${template}" != "mysql" ]; then
-    echo "## Testing ${template}:"
-    cp scripts/test.yaml humanitec-resource-defs/$template/basic/test-$template.yaml
-    cd humanitec-resource-defs/$template/basic
-    
-    template=$template yq -i '.entity.type = env(template)' test-$template.yaml
-    
-    yq -i '.entity.driver_inputs.values = load("definition-values.yaml")' test-$template.yaml
-    
-    tests=$(ls tests/*-inputs.yaml)
-    for test in $tests;
-    do
-      echo "## ${template} tested with ${test}."
-      humctl resources test-definition test-$template.yaml --inputs $test
-    done
+  echo "## Testing ${template}:"
+  cp scripts/test.yaml humanitec-resource-defs/$template/basic/test-$template.yaml
+  cd humanitec-resource-defs/$template/basic
+  
+  template=$template yq -i '.entity.type = env(template)' test-$template.yaml
+  
+  yq -i '.entity.driver_inputs.values = load("definition-values.yaml")' test-$template.yaml
+  
+  tests=$(ls tests/*-inputs.yaml)
+  for test in $tests;
+  do
+    echo "## ${template} tested with ${test}."
+    humctl resources test-definition test-$template.yaml --inputs $test
+  done
 
-    rm test-$template.yaml
-    cd ../../../
-  fi
+  rm test-$template.yaml
+  cd ../../../
 done
